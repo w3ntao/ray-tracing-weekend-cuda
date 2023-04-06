8c7ffca717a485b2fbfe73c1ec416788058c8ec0
Wentao Liu <wentao.liu.zero@outlook.com>
2023-04-06 11:16:50 +0800

    feature: add base/Color

---
+++ src/base/color.cu
@@ -0,0 +1,33 @@
+//
+// Created by wentao on 4/6/23.
+//
+
+#include "color.cuh"
+
+RGBColor RGBColor::operator+(const RGBColor &c) const { return RGBColor(r + c.r, g + c.g, b + c.b); }
+
+RGBColor RGBColor::operator-(const RGBColor &c) const { return RGBColor(r - c.r, g - c.g, b - c.b); }
+
+RGBColor RGBColor::operator*(const RGBColor &c) const { return RGBColor(r * c.r, g * c.g, b * c.b); }
+
+bool RGBColor::operator==(const RGBColor &c) const { return r == c.r && g == c.g && b == c.b; }
+
+bool RGBColor::operator!=(const RGBColor &b) const { return !(*this == b); }
+
+RGBColor RGBColor::clamp() const { return RGBColor(single_clamp(r), single_clamp(g), single_clamp(b)); }
+
+RGBColor operator*(float scalar, const RGBColor &c) {
+    return RGBColor(scalar * c.r, scalar * c.g, scalar * c.b);
+}
+
+RGBColor operator*(const RGBColor &c, float scalar) { return scalar * c; }
+
+void RGBColor::operator+=(const RGBColor &c) {
+    r += c.r;
+    g += c.g;
+    b += c.b;
+}
+
+RGBColor operator/(const RGBColor &c, float scalar) {
+    return RGBColor(c.r / scalar, c.g / scalar, c.b / scalar);
+}
---
+++ src/base/color.cuh
@@ -0,0 +1,46 @@
+//
+// Created by wentao on 4/6/23.
+//
+
+#ifndef CUDA_RAY_TRACER_COLOR_CUH
+#define CUDA_RAY_TRACER_COLOR_CUH
+
+class RGBColor {
+  public:
+    float r, g, b;
+
+    RGBColor() {}
+
+    RGBColor(float r, float g, float b) : r(r), g(g), b(b) {}
+
+    static RGBColor rep(float v) { return RGBColor(v, v, v); }
+
+    RGBColor operator+(const RGBColor &c) const;
+
+    void operator+=(const RGBColor &c);
+
+    RGBColor operator-(const RGBColor &c) const;
+
+    RGBColor operator*(const RGBColor &c) const;
+
+    bool operator==(const RGBColor &b) const;
+
+    bool operator!=(const RGBColor &b) const;
+
+    RGBColor clamp() const;
+
+    RGBColor gamma(float gam) const;
+
+    float luminance() const;
+
+  private:
+    inline float single_clamp(float x) const { return x < 0 ? 0 : (x > 1 ? 1 : x); }
+};
+
+RGBColor operator*(float scalar, const RGBColor &b);
+
+RGBColor operator*(const RGBColor &a, float scalar);
+
+RGBColor operator/(const RGBColor &a, float scalar);
+
+#endif // CUDA_RAY_TRACER_COLOR_CUH
